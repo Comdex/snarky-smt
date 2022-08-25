2eb0e881798ff55811bb25b95b3f31bbf602b7b4
Comdex <wcomdex@foxmail.com>
2022-08-25 18:32:34 +0800

    add support for rocksdb

---
+++ src/lib/store/rocks_store.ts
@@ -0,0 +1,212 @@
+import { AsFieldElements, Field } from 'snarkyjs';
+import { strToFieldArry } from '../utils';
+import { Store } from './store';
+import { FieldElements } from '../model';
+
+import levelup from 'levelup';
+
+/**
+ * Store based on rocksdb
+ *
+ * @export
+ * @class RocksStore
+ * @implements {Store<V>}
+ * @template V
+ */
+export class RocksStore<V extends FieldElements> implements Store<V> {
+  protected db: levelup.LevelUp;
+  protected batch: levelup.LevelUpChain;
+  protected nodesKey: string;
+  protected leavesKey: string;
+  protected eltTyp: AsFieldElements<V>;
+
+  /**
+   * Creates an instance of RocksStore.
+   * @param {levelup.LevelUp} db
+   * @param {AsFieldElements<V>} eltTyp
+   * @param {string} smtName
+   * @memberof RocksStore
+   */
+  constructor(
+    db: levelup.LevelUp,
+    eltTyp: AsFieldElements<V>,
+    smtName: string
+  ) {
+    this.db = db;
+    this.batch = db.batch();
+    this.nodesKey = smtName + ':';
+    this.leavesKey = smtName + '_leaf:';
+    this.eltTyp = eltTyp;
+  }
+
+  /**
+   * Clear all prepare operation cache.
+   *
+   * @memberof RocksStore
+   */
+  public clearPrepareOperationCache(): void {
+    this.batch = this.db.batch();
+  }
+
+  /**
+   * Get the tree root. Error is thrown when the root does not exist.
+   *
+   * @return {*}  {Promise<Field>}
+   * @memberof RocksStore
+   */
+  public async getRoot(): Promise<Field> {
+    const valueStr = await this.db.get(this.nodesKey + 'root');
+    return Field(valueStr);
+  }
+
+  /**
+   * Prepare update the root. Use the commit() method to actually submit changes.
+   *
+   * @param {Field} root
+   * @memberof RocksStore
+   */
+  public prepareUpdateRoot(root: Field): void {
+    this.batch = this.batch.put(this.nodesKey + 'root', root.toString());
+  }
+
+  /**
+   * Get nodes for a key. Error is thrown when a key that does not exist is being accessed.
+   *
+   * @param {Field} key
+   * @return {*}  {Promise<Field[]>}
+   * @memberof RocksStore
+   */
+  public async getNodes(key: Field): Promise<Field[]> {
+    const valueStr = await this.db.get(this.nodesKey + key.toString());
+
+    return strToFieldArry(valueStr);
+  }
+
+  /**
+   * Prepare put nodes for a key. Use the commit() method to actually submit changes.
+   *
+   * @param {Field} key
+   * @param {Field[]} value
+   * @memberof RocksStore
+   */
+  public preparePutNodes(key: Field, value: Field[]): void {
+    this.batch = this.batch.put(
+      this.nodesKey + key.toString(),
+      value.toString()
+    );
+  }
+
+  /**
+   * Prepare delete nodes for a key. Use the commit() method to actually submit changes.
+   *
+   * @param {Field} key
+   * @memberof RocksStore
+   */
+  public prepareDelNodes(key: Field): void {
+    this.batch = this.batch.del(this.nodesKey + key.toString());
+  }
+
+  /**
+   * Convert value string to a value of FieldElements type.
+   *
+   * @protected
+   * @param {string} valueStr
+   * @param {AsFieldElements<V>} eltTyp
+   * @return {*}  {V}
+   * @memberof RocksStore
+   */
+  protected strToValue(valueStr: string, eltTyp: AsFieldElements<V>): V {
+    let fs = strToFieldArry(valueStr);
+    return eltTyp.ofFields(fs);
+  }
+
+  /**
+   * Get the value for a key. Error is thrown when a key that does not exist is being accessed.
+   *
+   * @param {Field} path
+   * @return {*}  {Promise<V>}
+   * @memberof RocksStore
+   */
+  public async getValue(path: Field): Promise<V> {
+    const valueStr = await this.db.get(this.leavesKey + path.toString());
+
+    return this.strToValue(valueStr, this.eltTyp);
+  }
+
+  /**
+   * Serialize the value of the FieldElements type into a string
+   *
+   * @protected
+   * @param {V} value
+   * @return {*}  {string}
+   * @memberof RocksStore
+   */
+  protected valueToStr(value: V): string {
+    const valueStr = value.toFields().toString();
+    return valueStr;
+  }
+
+  /**
+   * Prepare put the value for a key. Use the commit() method to actually submit changes.
+   *
+   * @param {Field} path
+   * @param {V} value
+   * @memberof RocksStore
+   */
+  public preparePutValue(path: Field, value: V): void {
+    const valueStr = this.valueToStr(value);
+    this.batch = this.batch.put(this.leavesKey + path.toString(), valueStr);
+  }
+
+  /**
+   * Prepare delete the value for a key. Use the commit() method to actually submit changes.
+   *
+   * @param {Field} path
+   * @memberof RocksStore
+   */
+  public prepareDelValue(path: Field): void {
+    this.batch = this.batch.del(this.leavesKey + path.toString());
+  }
+
+  /**
+   * Use the commit() method to actually submit all prepare changes.
+   *
+   * @return {*}  {Promise<void>}
+   * @memberof RocksStore
+   */
+  public async commit(): Promise<void> {
+    if (this.batch.length > 0) {
+      await this.batch.write();
+    }
+
+    this.clearPrepareOperationCache();
+  }
+
+  /**
+   * Clear the store.
+   *
+   * @return {*}  {Promise<void>}
+   * @memberof RocksStore
+   */
+  public async clear(): Promise<void> {
+    await this.db.clear();
+  }
+
+  /**
+   * Get values map, key is Field.toString().
+   *
+   * @return {*}  {Promise<Map<string, V>>}
+   * @memberof RocksStore
+   */
+  public async getValuesMap(): Promise<Map<string, V>> {
+    let valuesMap = new Map<string, V>();
+    // @ts-ignore
+    for await (const [key, valueStr] of this.db.iterator()) {
+      const value = this.strToValue(valueStr, this.eltTyp);
+      const realKey = (key as string).replace(this.leavesKey, '');
+      valuesMap.set(realKey, value);
+    }
+
+    return valuesMap;
+  }
+}
---
+++ test/rocks_store.test.ts
@@ -0,0 +1,73 @@
+import { Field, isReady, shutdown } from 'snarkyjs';
+import { RocksStore } from '../src/lib/store/rocks_store';
+import encode from 'encoding-down';
+import rocksdb from 'rocksdb';
+import levelup from 'levelup';
+
+describe('RocksStore', () => {
+  let store: RocksStore<Field>;
+
+  // beforeAll(async () => {
+  //   await isReady;
+  // });
+
+  afterAll(async () => {
+    // `shutdown()` internally calls `process.exit()` which will exit the running Jest process early.
+    // Specifying a timeout of 0 is a workaround to defer `shutdown()` until Jest is done running all tests.
+    // This should be fixed with https://github.com/MinaProtocol/mina/issues/10943
+    setTimeout(shutdown, 0);
+  });
+
+  beforeEach(async () => {
+    await isReady;
+    const encoded = encode(rocksdb('./rocksdb'));
+    const db = levelup(encoded);
+    db.setMaxListeners(17);
+    store = new RocksStore<Field>(db, Field, 'test');
+  });
+
+  it('should set, get elements and update root correctly', async () => {
+    let keys: Field[] = [];
+    let nodes: Field[] = [];
+    let paths: Field[] = [];
+    let values: Field[] = [];
+    const updateTimes = 5;
+
+    for (let i = 0; i < updateTimes; i++) {
+      const key = Field(Math.floor(Math.random() * 1000000000000));
+      const node = Field(Math.floor(Math.random() * 1000000000000));
+      const path = Field(Math.floor(Math.random() * 1000000000000));
+      const value = Field(Math.floor(Math.random() * 1000000000000));
+      keys.push(key);
+      nodes.push(node);
+      paths.push(path);
+      values.push(value);
+
+      store.preparePutNodes(key, [node]);
+      store.preparePutValue(path, value);
+    }
+
+    const root = Field(999);
+    store.prepareUpdateRoot(root);
+
+    store.prepareDelNodes(keys[0]);
+    store.prepareDelValue(paths[0]);
+
+    await store.commit();
+
+    await expect(store.getNodes(keys[0])).rejects.toThrowError();
+
+    await expect(store.getValue(paths[0])).rejects.toThrowError();
+
+    const nodes1 = await store.getNodes(keys[1]);
+    expect(nodes1[0].equals(nodes[1]).toBoolean());
+
+    const value1 = await store.getValue(paths[1]);
+    expect(value1.equals(values[1]).toBoolean());
+
+    const updateRoot = await store.getRoot();
+    expect(updateRoot.equals(root).toBoolean());
+
+    expect(await store.getValuesMap()).toBeTruthy();
+  });
+});
